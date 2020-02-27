c50a7b3ad39cb661457d225d90ebdc5c0970fa7d
amit <gautamamits95@gmail.com>
2020-02-28 01:53:04 +0530

    project init

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node AS builder
+
+# Create app directory
+WORKDIR /app
+
+COPY package.json .
+RUN npm install
+
+COPY .  .
+
+FROM node
+ENV TINI_VERSION v0.18.0
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
+RUN chmod +x /tini
+ENTRYPOINT ["/tini", "--"]
+
+WORKDIR /app
+
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/views ./views
+COPY --from=builder /app/index.js ./index.js
+CMD ["node","index.js"]
---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const app= express();
+const fileUpload = require('express-fileupload');
+const PORT = Number(process.env.PORT) || 3000;
+const xunitViewer = require('xunit-viewer')
+
+app.use(express.json());
+app.set('view engine', 'pug');
+app.use(fileUpload());
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.post('/view', async (req, res)=>{
+    const result = await xunitViewer({
+        server: false,
+        title: 'Xunit View Sample Tests',
+        xml: req.body.xmlcontent || req.files.xml.data.toString()
+    })
+    res.send(result)
+})
+
+if (require.main === module) { // true if file is executed
+    app.listen(PORT, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('app listening on ', PORT);
+        }
+    });
+}
+exports.default=app
